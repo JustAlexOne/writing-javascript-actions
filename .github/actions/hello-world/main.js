0ff9070a6cc5d097746af5805628feb51dcc7e5f
Oleksii Cherevatyi <alex.cherevatiy@gmail.com>
2020-05-15 11:32:33 +0300

    Remove all input params

--- .github/actions/hello-world/main.js
+++ .github/actions/hello-world/main.js
@@ -5,9 +5,7 @@ const secondGreeting = core.getInput("second-greeting");
 const thirdGreeting = core.getInput("third-greeting");
 
 console.log(`Hello ${firstGreeting}`);
-core.info(`Hello ${firstGreeting}`)
 console.log(`Hello ${secondGreeting}`);
-core.info(`Hello ${firstGreeting}`);
 if (thirdGreeting) {
     console.log(`Hello ${thirdGreeting}`);
 }
