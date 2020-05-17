9906c7b3593395c41dab14c64298599d953c182c
JustAlexOne <alex.cherevatiy@gmail.com>
2020-05-17 09:34:33 +0300

    Add validation for input joke

--- .github/actions/issue-maker/index.js
+++ .github/actions/issue-maker/index.js
@@ -3,21 +3,25 @@ const github = require("@actions/github");
 
 async function run() {
     try {
-      const issueTitle = core.getInput("issue-title");
-      const jokeBody = core.getInput("joke");
-      const token = core.getInput("repo-token");
+        const issueTitle = core.getInput("issue-title");
+        const jokeBody = core.getInput("joke");
+        const token = core.getInput("repo-token");
 
-      const octokit = new github.GitHub(token);
+        console.log(`Joke: ${jokeBody}`);
+        if (!jokeBody) {
+            throw `Action input 'joke' is incorrect: ${jokeBody}`;
+        }
+        const octokit = new github.GitHub(token);
 
-      const newIssue = await octokit.issues.create({
-          repo: github.context.repo.repo,
-          owner: github.context.repo.owner,
-          title: issueTitle,
-          body: jokeBody
-      });
+        const newIssue = await octokit.issues.create({
+            repo: github.context.repo.repo,
+            owner: github.context.repo.owner,
+            title: issueTitle,
+            body: jokeBody
+        });
     } catch (error) {
         core.setFailed(error.message);
     }
-  }
+}
 
-  run();
+run();
