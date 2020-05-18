e0b98b25c2e9406e0cb33c0f833ff5ab9a46afcd
JustAlexOne <alex.cherevatiy@gmail.com>
2020-05-18 15:21:43 +0300

    add fs.readdir method

--- .github/actions/hello-world/main.js
+++ .github/actions/hello-world/main.js
@@ -1,4 +1,7 @@
 const core = require("@actions/core");
+const path = require('path');
+const fs = require('fs');
+
 
 const firstGreeting = core.getInput("first-greeting");
 const secondGreeting = core.getInput("second-greeting");
@@ -9,3 +12,17 @@ console.log(`Hello ${secondGreeting}`);
 if (thirdGreeting) {
     console.log(`Hello ${thirdGreeting}`);
 }
+
+const directoryPath = './';
+// const directoryPath = path.join(__dirname, 'Document');
+
+fs.readdir(directoryPath, function (err, files) {
+    console.log('Reading files in dir: ' + directoryPath);
+    if (err) {
+        return console.log('Unable to scan directory: ' + err);
+    }
+
+    files.forEach(function (file) {
+        console.log('File: ' + file);
+    });
+});
